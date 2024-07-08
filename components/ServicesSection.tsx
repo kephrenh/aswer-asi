a6d7889b00243f2e2b7ffbb0d1ab410b66fd3b99
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-07-09 01:29:53 +0200

    create Services section component

---
+++ components/ServicesSection.tsx
@@ -0,0 +1,8 @@
+const ServicesSection = () => {
+  return (
+    <section className="h-screen absolute z-50 bg-white w-full">
+      <h2>Nos services</h2>
+    </section>
+  );
+};
+export default ServicesSection;
