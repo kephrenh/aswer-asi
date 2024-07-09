de85d90ed4bd7defb24813fb4a1038a893b948eb
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-07-09 19:24:11 +0200

    modify footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
   }, []);
 
   return (
-    <footer className=" flex flex-col justify-center relative  z-50 text-gray-100 w-full">
+    <footer className=" flex flex-col justify-center relative bg-gray-900/80 z-50 text-gray-50 w-full">
       <div className="absolute w-full h-full backdrop-brightness-50 z-10"></div>
 
       <div className="mx-auto z-50">
@@ -33,10 +33,10 @@ const Footer: React.FC = () => {
         />
       </div>
       <address className="mx-auto w-full flex flex-col z-50">
-        <h2 className="text-center pb-8">Nos coordonnées</h2>
+        <h2 className="text-center pb-8 tracking-widest">Nos coordonnées</h2>
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
           <section className="flex flex-col gap-2 mx-auto">
-            <h3 className="text-center lg:text-start underline-offset-2 underline ">
+            <h3 className="text-center lg:text-start underline-offset-2 underline tracking-widest">
               Appelez-nous
             </h3>
             <div className="flex gap-2">
@@ -44,33 +44,37 @@ const Footer: React.FC = () => {
                 className="text-blue-800"
                 size={30}
               />
-              <div className="flex flex-col gap-1 font-bold">
+              <div className="flex flex-col gap-1 text-sm tracking-wider">
                 <a href="tel:+33651464424">+33.6.51.46.64.24</a>
                 <a href="tel:+33619908273">+33.6.19.90.82.73</a>
               </div>
             </div>
           </section>
           <section className="flex flex-col gap-2 mx-auto">
-            <h3 className="text-center underline-offset-2 underline lg:text-start">Siège Social</h3>
+            <h3 className="text-center underline-offset-2 underline lg:text-start tracking-widest">
+              Siège Social
+            </h3>
             <div className="flex gap-2">
               <MapPin
                 className="text-blue-800"
                 size={30}
               />
-              <div className="flex flex-col gap-1 font-bold">
-                <span>36 rue Rochebrune</span>
+              <div className="flex flex-col gap-1 text-sm tracking-wider">
+                <span>56 rue Rochebrune</span>
                 <span>93100 Montreuil</span>
               </div>
             </div>
           </section>
           <section className="flex flex-col gap-2 mx-auto">
-            <h3 className="text-center underline-offset-2 underline lg:text-start">Ecrivez-nous</h3>
+            <h3 className="text-center underline-offset-2 underline lg:text-start tracking-widest">
+              Ecrivez-nous
+            </h3>
             <div className="flex gap-2">
               <Mail
                 className="text-blue-800"
                 size={30}
               />
-              <div className="flex flex-col gap-1 font-bold">
+              <div className="flex flex-col gap-1 text-sm tracking-wider">
                 <a href="mailto:prezyaswer13@gmail.com">prezyaswer13@gmail.com</a>
                 <a href="mailto:prezy_awer@yahoo.fr">prezy_awer@yahoo.fr</a>
               </div>
@@ -83,7 +87,6 @@ const Footer: React.FC = () => {
         <p>ASI - {"Entreprise de sécurité incendie et d'aide aux personnes"}</p>
         <p>SIREN: 917 945 495</p>
         <ul className="flex justify-center gap-2">
-          <li></li>
           <li>
             <Link
               className="underline"
@@ -101,7 +104,7 @@ const Footer: React.FC = () => {
         </ul>
       </div>
       <div
-        className={`fixed bottom-8 right-8 hover:bg-blue-700/85 rounded-full transition-all duration-300 z-50 p-2 bg-gray-900/85 text-gray-100 cursor-pointer ${
+        className={`fixed bottom-8 right-8 hover:bg-blue-700/85 rounded-full transition-all duration-300 z-50 p-2 bg-blue-900/85 text-gray-100 cursor-pointer ${
           arrow ? "opacity-100" : "opacity-0 translate-y-10"
         }`}>
         <a href="#accueil">
