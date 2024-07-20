5e9e032b985177d10f4253e04d695dc3e2b111b7
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-07-20 13:04:59 +0200

    implement services section

---
+++ app/(front)/page.tsx
@@ -0,0 +1,10 @@
+import ServicesSection from "@components/ServicesSection";
+import { Button } from "@components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="h-full w-full bg-gray-100/90 py-16">
+      <ServicesSection />
+    </div>
+  );
+}
--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -1,7 +1,37 @@
+import { services } from "@lib/data";
+import Image from "next/image";
+
 const ServicesSection = () => {
   return (
-    <section className="h-screen absolute z-50 bg-white w-full">
+    <section className="w-full gap-5 flex flex-col items-center">
       <h2>Nos services</h2>
+      <div className="grid gap-8">
+        {services.map((service) => (
+          <article
+            // style={{
+            //   backgroundImage: `url(${service.image})`,
+            //   backgroundSize: "cover",
+            //   backgroundPosition: "center",
+            //   backgroundRepeat: "no-repeat",
+            //   filter: "brightness(50%)",
+            // }}
+            key={service.id}
+            className="flex group flex-col h-auto items-center gap-4 relative rounded-lg bg-gray-200">
+            <div className="absolute w-full h-full">
+              <Image
+                src={service.image}
+                alt="image sécurité incendie"
+                fill
+                className="object-cover brightness-50 rounded-xl duration-300 group-hover:scale-105"
+              />
+            </div>
+            <div className="text-center text-white group-hover:scale-105 duration-300 z-50 w-[50%]">
+              <h3 className="text-center pt-5 text-2xl pb-4">{service.name}</h3>
+              <p className="px-5 pb-5">{service.desc}</p>
+            </div>
+          </article>
+        ))}
+      </div>
     </section>
   );
 };
